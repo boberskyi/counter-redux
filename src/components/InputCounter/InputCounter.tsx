8123496535ebbdfb87fc4e9433c72f49095f8045
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-10-02 19:41:09 +0200

    universal input

--- src/components/InputCounter/InputCounter.tsx
+++ src/components/InputCounter/InputCounter.tsx
@@ -1,51 +1,29 @@
 import React, {ChangeEvent, useState} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "../../redux/store";
 import {StyledSettingsInput, StyledSettingsLabel, StyledSettingsRow} from "./InputCounterStyles";
-import {setMaxValueAC, setMinValueAC, setStartValueAC} from "../../redux/actions/actions";
 
 type InputCounterPropsType = {
-    disableActiveBtn: () => void
-    name: 'min' | 'max'
+    callback: (text:string) => void
+    value: string
+    error: string | undefined
+    text: string
+    min?: number
+    max?: number
 }
-export const InputCounter: React.FC<InputCounterPropsType> = ({name, ...props}) => {
-    const dispatch = useDispatch();
-    const counterMaxValue = useSelector<AppRootStateType, number>(store => store.counter.maxValue);
-    const counterStartValue = useSelector<AppRootStateType, number>(store => store.counter.startValue);
+export const InputCounter: React.FC<InputCounterPropsType> = ({...props}) => {
+    const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => props.callback(e.currentTarget.value);
 
-    const storedStartValue = Number(localStorage.getItem('startValue'));
-    const [newMinVal, setNewMinVal] = useState(storedStartValue ? storedStartValue : counterStartValue);
-    const onMinInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        props.disableActiveBtn();
-        dispatch(setMinValueAC(Number(e.currentTarget.value)));
-        dispatch(setStartValueAC(Number(e.currentTarget.value)));
-        setNewMinVal(Number(e.currentTarget.value));
-    }
-    const onMaxInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        props.disableActiveBtn();
-        dispatch(setMaxValueAC(Number(e.currentTarget.value)));
-    }
 
-    switch (name) {
-        case "max": return (
-            <StyledSettingsRow>
-                <StyledSettingsLabel>
-                    max value:
-                    <StyledSettingsInput value={counterMaxValue} min={counterStartValue} onChange={onMaxInputChange}
-                                         type='number'
-                                         error={counterStartValue >= counterMaxValue ? 'true' : undefined}/>
-                </StyledSettingsLabel>
-            </StyledSettingsRow>
-            );
-        case "min": return (
-            <StyledSettingsRow>
-                <StyledSettingsLabel>
-                    start value:
-                    <StyledSettingsInput value={newMinVal} min='0' max={counterMaxValue} onChange={onMinInputChange}
-                                         type='number'
-                                         error={counterStartValue >= counterMaxValue ? 'true' : undefined}/>
-                </StyledSettingsLabel>
-            </StyledSettingsRow>
-            );
-    }
+    return (
+        <StyledSettingsRow>
+            <StyledSettingsLabel>
+                {props.text}
+                <StyledSettingsInput value={props.value}
+                                     min={props.min}
+                                     max={props.max}
+                                     onChange={onChangeInput}
+                                     type='number'
+                                     error={props.error}/>
+            </StyledSettingsLabel>
+        </StyledSettingsRow>
+    )
 };
--- src/components/LeftCounterFrame/LeftCounterFrame.tsx
+++ src/components/LeftCounterFrame/LeftCounterFrame.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../redux/store";
 import {BtnCounter} from "../BtnCounter/BtnCounter";
 import {
@@ -8,8 +8,11 @@ import {
     StyledSettings
 } from "./LeftCounterFrameStyles";
 import {InputCounter} from "../InputCounter/InputCounter";
+import {setMaxValueAC, setMinValueAC, setStartValueAC} from "../../redux/actions/actions";
 
 export const LeftCounterFrame: React.FC = () => {
+    const dispatch = useDispatch();
+
     const counterStartValue = useSelector<AppRootStateType, number>(store => store.counter.startValue);
     const counterMinValue = useSelector<AppRootStateType, number>(store => store.counter.minValue);
     const counterMaxValue = useSelector<AppRootStateType, number>(store => store.counter.maxValue);
@@ -17,17 +20,39 @@ export const LeftCounterFrame: React.FC = () => {
     const [activeBtn, setActiveBtn] = useState(false);
     const onBtnSetClick = () => {
         setActiveBtn(true);
-        localStorage.setItem('startValue', counterStartValue.toString());
-        localStorage.setItem('minValue', counterMinValue.toString());
-        localStorage.setItem('maxValue', counterMaxValue.toString());
+        localStorage.setItem('startValue', JSON.stringify(counterStartValue));
+        localStorage.setItem('minValue', JSON.stringify(counterMinValue));
+        localStorage.setItem('maxValue', JSON.stringify(counterMaxValue));
     }
     const disableActiveBtn = () => setActiveBtn(false);
+
+    const onMinInputChange = (text:string) => {
+        disableActiveBtn();
+        dispatch(setMinValueAC(Number(text)));
+        dispatch(setStartValueAC(Number(text)));
+    }
+    const onMaxInputChange = (text:string) => {
+        disableActiveBtn();
+        dispatch(setMaxValueAC(Number(text)));
+    }
+
 
     return (
         <StyledLeftCounterFrame>
             <StyledSettings>
-                <InputCounter disableActiveBtn={disableActiveBtn} name={'max'}/>
-                <InputCounter disableActiveBtn={disableActiveBtn} name={'min'}/>
+                <InputCounter callback={onMaxInputChange}
+                              value={counterMaxValue.toString()}
+                              text={'max value: '}
+                              min={counterMinValue}
+                              error={counterStartValue >= counterMaxValue ? 'true' : undefined}
+                />
+                <InputCounter callback={onMinInputChange}
+                              value={counterMinValue.toString()}
+                              text={'start value: '}
+                              min={0}
+                              max={counterMaxValue}
+                              error={counterStartValue >= counterMaxValue ? 'true' : undefined}
+                />
             </StyledSettings>
 
             <StyledLeftControls>
