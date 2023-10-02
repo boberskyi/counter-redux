f9cb91fdec41ad8ea404061d61990a030ef9fe19
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-10-02 19:40:54 +0200

    Local storage fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import '../../App.css';
 import {RightCounterFrame} from "../RightCounterFrame/RightCounterFrame";
 import {LeftCounterFrame} from "../LeftCounterFrame/LeftCounterFrame";
-import {useDispatch} from "react-redux";
 import {StyledApp} from "./AppStyles";
-import {setMaxValueAC, setMinValueAC, setStartValueAC} from "../../redux/actions/actions";
 
 const App = () => {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        const storedStartValue = Number(localStorage.getItem('startValue'));
-        const storedMinValue = Number(localStorage.getItem('minValue'));
-        const storedMaxValue = Number(localStorage.getItem('maxValue'));
-
-        storedStartValue && dispatch(setStartValueAC(storedStartValue));
-        storedMinValue && dispatch(setMinValueAC(storedMinValue));
-        storedMaxValue && dispatch(setMaxValueAC(storedMaxValue));
-    }, []);
-
-
     return (
         <StyledApp className="App">
             <LeftCounterFrame/>
--- src/redux/reducers/counter-reducer.ts
+++ src/redux/reducers/counter-reducer.ts
@@ -2,11 +2,16 @@ import {storeType} from "../store";
 import {allCounterTypes} from "../actions/actions";
 import {INCREMENT_VALUE, RESET_VALUE, SET_MAX_VALUE, SET_MIN_VALUE, SET_START_VALUE} from "../actions/actionTypes";
 
+const storedStartValue = Number(localStorage.getItem('startValue'));
+const storedMinValue = Number(localStorage.getItem('minValue'));
+const storedMaxValue = Number(localStorage.getItem('maxValue'));
+
 const initialState:storeType = {
-    startValue: 0,
-    minValue: 0,
-    maxValue: 1
+    startValue: storedStartValue ? storedStartValue : 1,
+    minValue: storedMinValue ? storedMinValue : 1,
+    maxValue: storedMaxValue ? storedMaxValue : 5
 };
+
 
 export const counterReducer = (state = initialState, action: allCounterTypes) => {
     switch (action.type) {
